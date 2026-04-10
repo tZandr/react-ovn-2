5627dbc65dbe3a18227bb9edc00cd0881f7f49e5
tZandr <alex_tjernstrom@hotmail.com>
2026-04-10 10:40:16 +0200

    extra: conditional rendering

--- src/components/temperature.tsx
+++ src/components/temperature.tsx
@@ -10,6 +10,19 @@ export const ShowTemp = () => {
         <button onClick={() => setCount((prev) => prev - 1)}>-</button>
         <button onClick={() => setCount((prev) => prev + 1)}>+</button>
       </div>
+      <br />
+      {temp > 15 && temp <= 30 && <p>Sommar</p>}
+      {temp >= 5 && temp <= 15 && <p>Höst/Vår</p>}
+      {temp < 5 && <p>Vinter</p>}
+      {temp > 30 && (
+        <small>
+          Remember that level in Super Mario Bros. 3, where the sun is trying to
+          kill you? <br />
+          That's today.
+        </small>
+      )}
     </div>
   );
 };
+
+// Glömde bort att extrauppgifterna var > 25 och < 10 grader
