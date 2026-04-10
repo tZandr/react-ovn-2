ccb02980a2f83bfe515149c260e2ea22e4b966b1
tZandr <alex_tjernstrom@hotmail.com>
2026-04-10 10:48:23 +0200

    hide/show profilecard conditional render

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import { ProfileCard } from './components/ProfileCard';
 import { ShowTemp } from './components/temperature';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [showComponent, setShowComponent] = useState<boolean>(true);
 
   useEffect(() => {
     console.log('App component has been rendered');
   }, []);
-  
+
   return (
     <div className="root">
       <h1>React Övning 2</h1>
       <div>
-        <ProfileCard name="Alexander" occupation="AppDev" city="Stockholm" />
+        <button onClick={() => setShowComponent((prev) => !prev)}>
+          {showComponent ? 'Hide ' : 'Show '}
+          Profile
+        </button>
+        {showComponent && (
+          <ProfileCard name="Alexander" occupation="AppDev" city="Stockholm" />
+        )}
       </div>
       <div>
         <ShowTemp />
